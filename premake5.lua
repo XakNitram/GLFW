85beab7ba62780e8a7a40f8a4992b24b6665ef27
Tate Mioton <tatemioton@outlook.com>
2020-10-03 22:24:45 -0500

    Allow more customization of output directories

--- premake5.lua
+++ premake5.lua
@@ -2,8 +2,8 @@ project "GLFW"
 	kind "StaticLib"
 	language "C"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir (builddir)
+	objdir (intermediatesdir)
 
 	files
 	{
